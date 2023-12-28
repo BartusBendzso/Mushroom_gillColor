4ad4c99950102ee87b2cf28d163f7a6c306581e5
BartusBendzso <77012994+BartusBendzso@users.noreply.github.com>
2023-12-28 22:55:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from prediction import predict
-
+import sklearn
 
 st.title('Classifying Mushrooms')
 st.markdown('Predicting Mushrooms Gill-color')
